e6f602d64f841dc657ebc0a3c02453f076725ea3
Tina Hang <mstinahang@gmail.com>
2018-08-25 00:35:25 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,13 +3,13 @@
 var turn = 0;
 let gameId = 0;
 const winCombinations= [
-    [0,1,2], /*top row*/ [3,4,5], /* middle row*/ [6,7,8], /*bottom row*/
-    [0,3,6], /*1st col*/ [1,4,7], /*2nd col*/ [2,5,8], /*3rd col*/
-    [0,4,8], /*neg diag*/ [2,4,6] /*pos diag*/
+    [0,1,2], [3,4,5], [6,7,8], 
+    [0,3,6], [1,4,7], [2,5,8], 
+    [0,4,8], [2,4,6] 
 ];
 
 function player () {
-    return ((turn%2 === 0) ? 'X' : 'O');
+    return ((turn % 2 === 0) ? 'X' : 'O');
 };
 
 function updateState (square) {
